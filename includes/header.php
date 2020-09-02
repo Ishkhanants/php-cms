39972a80f17c70fa279a6f7e082f8e0c6f79e8f2
Martin Mirzoyan <Martin_Mirzoyan@epam.com>
2020-09-02 14:19:28 +0400

    Added the project

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="Chi Lin" >
+		<title><?php echo SITENAME;?></title>
+		<link rel="icon" href="/favicon.png" type="image/png" sizes="16x16">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/blog-home.css" rel="stylesheet">
+	</head>
+	<body>
